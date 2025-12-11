2a428b33e20afabca53e7cdef9c7ded66cd6e23f
Jonathan GANIVET <jon.ganivet@gmail.com>
2025-12-11 07:45:24 +0100

    Second exercise

---
+++ partie1-03.js
@@ -0,0 +1,10 @@
+let totalBooks = 500;
+totalBooks += 50;
+totalBooks -= 10;
+totalBooks += 5;
+console.log(totalBooks);
+
+let showBooks = "We have a total of ";
+showBooks += totalBooks;
+showBooks += " books in our library.";
+console.log(showBooks);
